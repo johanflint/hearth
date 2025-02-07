9b9672cc97748957fd4cf5695f1c55f7c961ff5b
Johan Flint <johan.flint@gmail.com>
2025-02-07 13:32:47 +0100

    Move config to a separate file

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,22 @@
+use crate::app_config::AppConfig;
 use crate::sse_listen::listen;
 use reqwest::Client;
+use tracing::info;
 
+mod app_config;
 mod sse_listen;
 
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt().with_max_level(tracing::Level::INFO).init();
 
+    let config = AppConfig::load();
+    info!("Loaded configuration file: {:?}", &config);
+
     let client = Client::builder()
         .danger_accept_invalid_certs(true)
         .build()
         .expect("Failed to build client");
 
-    listen(&client, "https://192.168.1.150/eventstream/clip/v2")
-        .await
-        .expect("Could not listen to SSE stream");
+    listen(&client, &config).await.expect("Could not listen to SSE stream");
 }
--- src/sse_listen.rs
+++ src/sse_listen.rs
@@ -1,3 +1,5 @@
+use crate::app_config::AppConfig;
+use config::Config;
 use futures::StreamExt;
 use reqwest::{Client, StatusCode};
 use std::error::Error;
@@ -7,16 +9,16 @@ use tokio_retry::strategy::{jitter, ExponentialBackoff};
 use tokio_retry::Retry;
 use tracing::{error, info, instrument, warn};
 
-#[instrument(skip(client), fields(url = %url.as_ref()))]
-pub async fn listen(client: &Client, url: impl AsRef<str>) -> Result<(), Box<dyn Error>> {
-    let strategy = ExponentialBackoff::from_millis(500)
+#[instrument(skip(client, config))]
+pub async fn listen(client: &Client, config: &AppConfig) -> Result<(), Box<dyn Error>> {
+    let strategy = ExponentialBackoff::from_millis(config.hue().retry_ms())
         .factor(2)
-        .max_delay(Duration::from_secs(30))
+        .max_delay(config.hue().max_delay_ms())
         .map(jitter);
 
-    info!("Connecting to SSE stream {}...", url.as_ref());
+    info!("Connecting to SSE stream {}...", config.hue().url());
     Retry::spawn(strategy, || async {
-        match connect_sse_stream(&client, url.as_ref()).await {
+        match connect_sse_stream(&client, config).await {
             Ok(_) => {
                 info!("✅ SSE stream ended gracefully. Restarting...");
                 Err("Stream ended") // Triggers retry
@@ -32,24 +34,24 @@ pub async fn listen(client: &Client, url: impl AsRef<str>) -> Result<(), Box<dyn
     Ok(())
 }
 
-#[instrument(skip(client, url))]
-async fn connect_sse_stream(client: &Client, url: impl AsRef<str>) -> Result<(), Box<dyn Error>> {
+#[instrument(skip(client, config))]
+async fn connect_sse_stream(client: &Client, config: &AppConfig) -> Result<(), Box<dyn Error>> {
+    let url = format!("{}/eventstream/clip/v2", config.hue().url());
     let response = client
-        .get(url.as_ref())
+        .get(&url)
         .header("Accept", "text/event-stream")
+        .header("hue-application-key", config.hue().application_key())
         .send()
         .await?
         .error_for_status()?;
 
     if response.status() == StatusCode::OK {
-        info!(status = %response.status(), "Connecting to SSE stream {}... OK", url.as_ref());
+        info!(status = %response.status(), "Connecting to SSE stream {}... OK", config.hue().url());
     }
 
     let mut stream = response.bytes_stream();
-    let timeout_duration = Duration::from_secs(30);
-
     loop {
-        let event = timeout(timeout_duration, stream.next()).await;
+        let event = timeout(config.hue().max_delay_ms(), stream.next()).await;
         match event {
             Ok(Some(Ok(chunk))) => {
                 if let Ok(text) = String::from_utf8(chunk.to_vec()) {
@@ -70,7 +72,7 @@ async fn connect_sse_stream(client: &Client, url: impl AsRef<str>) -> Result<(),
                 return Err("Stream closed".into());
             }
             Err(_) => {
-                warn!("⏳ No data for {} seconds. Reconnecting...", timeout_duration.as_secs());
+                warn!("⏳ No data for {} seconds. Reconnecting...", config.hue().max_delay_ms().as_secs());
                 return Err("Timeout".into());
             }
         }
