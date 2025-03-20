cadb28dc879b8243706c46d67f3316390041d01a
Johan Flint <johan.flint@gmail.com>
2025-03-20 16:33:17 +0100

    Update brightness property for lights

--- src/hue/map_light_changed.rs
+++ src/hue/map_light_changed.rs
@@ -1,13 +1,22 @@
 use crate::domain::events::Event;
+use crate::domain::property::Number;
 use crate::hue::domain::LightChanged;
 
 pub fn map_light_changed_property(property: LightChanged) -> Vec<Event> {
-    let mut events = Vec::<Event>::with_capacity(1);
+    let mut events = Vec::<Event>::with_capacity(2);
     if let Some(on) = property.on {
         events.push(Event::BooleanPropertyChanged {
             device_id: property.owner.rid.to_string(),
             property_id: "on".to_string(),
             value: on.on,
+        });
+    }
+
+    if let Some(dimming) = property.dimming {
+        events.push(Event::NumberPropertyChanged {
+            device_id: property.owner.rid.to_string(),
+            property_id: "brightness".to_string(),
+            value: Number::Float(dimming.brightness),
         });
     }
 
