eb38be3bd9431ae0b76779ce0f90b077e10e8582
Johan Flint <johan.flint@gmail.com>
2025-03-25 13:50:01 +0100

    Introduce property values

--- src/flow_engine/mod.rs
+++ src/flow_engine/mod.rs
@@ -2,6 +2,7 @@ pub mod action;
 mod action_registry;
 mod engine;
 pub mod flow;
+pub mod property_value;
 
-pub use engine::execute;
 pub use engine::FlowEngineError;
+pub use engine::execute;
--- src/flow_loader/mod.rs
+++ src/flow_loader/mod.rs
@@ -1,5 +1,6 @@
 mod factory;
 mod loader;
+mod property_value_deserializer;
 pub(in crate::flow_loader) mod serialized_flow;
 
 pub use loader::load_flows_from;
