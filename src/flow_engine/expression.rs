d5688caa86f5062ebffd131a5ef03d811f3184e4
Johan Flint <johan.flint@gmail.com>
2025-07-01 10:20:44 +0200

    Improve expression evaluation with error handling
    Enhances expression evaluation by introducing a custom error type, `ExpressionError`, for more informative error reporting.
    
    Replaces the generic `&'static str` error type with `ExpressionError` to provide specific error variants for different failure scenarios, such as unknown devices, unknown properties, and type mismatches.
    
    This change allows for more robust error handling and debugging during flow execution.

--- src/flow_engine/expression.rs
+++ src/flow_engine/expression.rs
@@ -1,9 +1,12 @@
 use crate::domain::Number;
 use crate::domain::property::{BooleanProperty, NumberProperty, PropertyType};
 use crate::flow_engine::Context;
+use crate::flow_engine::expression::ExpressionError::UnknownProperty;
 use std::cmp::Ordering;
+use thiserror::Error;
 use tracing::warn;
 
+#[derive(Debug)]
 pub enum Expression {
     // Comparison
     GreaterThanOrEqualTo(Box<Expression>, Box<Expression>),
@@ -28,7 +31,7 @@ pub enum Value {
     Number(Number),
 }
 
-pub fn evaluate(expression: &Expression, context: &Context) -> Result<Value, &'static str> {
+pub fn evaluate(expression: &Expression, context: &Context) -> Result<Value, ExpressionError> {
     use Expression::*;
 
     match expression {
@@ -42,12 +45,22 @@ pub fn evaluate(expression: &Expression, context: &Context) -> Result<Value, &'s
         EqualTo(lhs, rhs) => match (evaluate(lhs, context)?, evaluate(rhs, context)?) {
             (Value::Number(a), Value::Number(b)) => Ok(Value::Boolean(a.eq(&b))),
             (Value::Boolean(a), Value::Boolean(b)) => Ok(Value::Boolean(a == b)),
-            _ => Err("Invalid expression, must compare two values of the same type"),
+            _ => Err(ExpressionError::OperandTypeMismatch {
+                operand: "EqualTo",
+                expected: "Boolean|Number",
+                actual_lhs: format!("{:?}", lhs),
+                actual_rhs: format!("{:?}", rhs),
+            }),
         },
         NotEqualTo(lhs, rhs) => match (evaluate(lhs, context)?, evaluate(rhs, context)?) {
             (Value::Number(a), Value::Number(b)) => Ok(Value::Boolean(!a.eq(&b))),
             (Value::Boolean(a), Value::Boolean(b)) => Ok(Value::Boolean(a != b)),
-            _ => Err("Invalid expression, must compare two values of the same type"),
+            _ => Err(ExpressionError::OperandTypeMismatch {
+                operand: "NotEqualTo",
+                expected: "Boolean|Number",
+                actual_lhs: format!("{:?}", lhs),
+                actual_rhs: format!("{:?}", rhs),
+            }),
         },
 
         // Literal
@@ -57,12 +70,15 @@ pub fn evaluate(expression: &Expression, context: &Context) -> Result<Value, &'s
         PropertyValue { device_id, property_id } => {
             let Some(device) = context.snapshot().devices.get(device_id) else {
                 warn!(device_id, "⚠️ Received property changed event for unknown device '{}'", device_id);
-                return Err("Unknown device");
+                return Err(ExpressionError::UnknownDevice(device_id.clone()));
             };
 
             let Some(property) = device.properties.get(property_id) else {
                 warn!(device_id = device.id, "⚠️ Unknown property '{}' for device '{}'", property_id, device.name);
-                return Err("Unknown property");
+                return Err(UnknownProperty {
+                    device_id: device_id.clone(),
+                    property_id: property_id.clone(),
+                });
             };
 
             match property.property_type() {
@@ -70,8 +86,8 @@ pub fn evaluate(expression: &Expression, context: &Context) -> Result<Value, &'s
                     let value = property.as_any().downcast_ref::<NumberProperty>().unwrap();
                     Ok(Value::Number(value.value()))
                 }
-                PropertyType::Color => Err("Unsupported property type"),
-                PropertyType::ColorTemperature => Err("Unsupported property type"),
+                PropertyType::Color => Err(ExpressionError::UnsupportedPropertyType(property.property_type())),
+                PropertyType::ColorTemperature => Err(ExpressionError::UnsupportedPropertyType(property.property_type())),
                 PropertyType::On => {
                     let value = property.as_any().downcast_ref::<BooleanProperty>().unwrap();
                     Ok(Value::Boolean(value.value()))
@@ -81,11 +97,38 @@ pub fn evaluate(expression: &Expression, context: &Context) -> Result<Value, &'s
     }
 }
 
-fn compare(lhs: &Expression, rhs: &Expression, cmp: fn(Ordering) -> bool, context: &Context) -> Result<Value, &'static str> {
+fn compare(lhs: &Expression, rhs: &Expression, cmp: fn(Ordering) -> bool, context: &Context) -> Result<Value, ExpressionError> {
     match (evaluate(lhs, context)?, evaluate(rhs, context)?) {
-        (Value::Number(a), Value::Number(b)) => Ok(Value::Boolean(cmp(a.partial_cmp(&b).ok_or("not a number")?))),
-        _ => Err("Comparison failed: expected numbers"),
+        (Value::Number(a), Value::Number(b)) => Ok(Value::Boolean(cmp(a.partial_cmp(&b).ok_or_else(|| ExpressionError::ComparisonFailed {
+            actual_lhs: format!("{:?}", lhs),
+            actual_rhs: format!("{:?}", rhs),
+        })?))),
+        _ => Err(ExpressionError::OperandTypeMismatch {
+            operand: "Compare",
+            expected: "Number",
+            actual_lhs: format!("{:?}", lhs),
+            actual_rhs: format!("{:?}", rhs),
+        }),
     }
+}
+
+#[derive(Error, PartialEq, Debug)]
+pub enum ExpressionError {
+    #[error("operand type mismatch for operand {operand}")]
+    OperandTypeMismatch {
+        operand: &'static str,
+        expected: &'static str,
+        actual_lhs: String,
+        actual_rhs: String,
+    },
+    #[error("unknown device '{0}'")]
+    UnknownDevice(String),
+    #[error("unknown property '{property_id}' for device '{device_id}'")]
+    UnknownProperty { device_id: String, property_id: String },
+    #[error("property type '{0:?}' is not supported as a property value")]
+    UnsupportedPropertyType(PropertyType),
+    #[error("unable to compare given Numbers {actual_lhs} and {actual_rhs}")]
+    ComparisonFailed { actual_lhs: String, actual_rhs: String },
 }
 
 #[cfg(test)]
@@ -245,6 +288,28 @@ mod tests {
     }
 
     #[rstest]
+    #[case(true, true, true)]
+    #[case(true, false, false)]
+    #[case(false, true, false)]
+    #[case(false, false, true)]
+    fn equal_to_bool(#[case] lhs: bool, #[case] rhs: bool, #[case] expected: bool) {
+        let result = evaluate(&EqualTo(Box::new(Literal(Value::Boolean(lhs))), Box::new(Literal(Value::Boolean(rhs)))), &context()).unwrap();
+        assert_eq!(result, Value::Boolean(expected));
+    }
+
+    #[rstest]
+    #[case(Value::Boolean(true), Value::Number(Number::PositiveInt(2)), ExpressionError::OperandTypeMismatch {
+                operand: "EqualTo",
+                expected: "Boolean|Number",
+                actual_lhs: "Literal(Boolean(true))".to_string(),
+                actual_rhs: "Literal(Number(PositiveInt(2)))".to_string(),
+        })]
+    fn equal_to_mismatch(#[case] lhs: Value, #[case] rhs: Value, #[case] expected: ExpressionError) {
+        let result = evaluate(&EqualTo(Box::new(Literal(lhs)), Box::new(Literal(rhs))), &context()).unwrap_err();
+        assert_eq!(result, expected);
+    }
+
+    #[rstest]
     #[case(Number::PositiveInt(2), Number::PositiveInt(5), true)]
     #[case(Number::PositiveInt(5), Number::PositiveInt(2), true)]
     #[case(Number::PositiveInt(5), Number::PositiveInt(5), false)]
@@ -261,11 +326,39 @@ mod tests {
     }
 
     #[rstest]
-    #[case::unknown_device("unknown_device_id", "", Err("Unknown device"))]
-    #[case::unknown_property("42", "unknown_property_id", Err("Unknown device"))]
+    #[case(true, true, false)]
+    #[case(true, false, true)]
+    #[case(false, true, true)]
+    #[case(false, false, false)]
+    fn not_equal_to_bool(#[case] lhs: bool, #[case] rhs: bool, #[case] expected: bool) {
+        let result = evaluate(&NotEqualTo(Box::new(Literal(Value::Boolean(lhs))), Box::new(Literal(Value::Boolean(rhs)))), &context()).unwrap();
+        assert_eq!(result, Value::Boolean(expected));
+    }
+
+    #[rstest]
+    #[case(Value::Boolean(true), Value::Number(Number::PositiveInt(2)), ExpressionError::OperandTypeMismatch {
+                operand: "NotEqualTo",
+                expected: "Boolean|Number",
+                actual_lhs: "Literal(Boolean(true))".to_string(),
+                actual_rhs: "Literal(Number(PositiveInt(2)))".to_string(),
+        })]
+    fn not_equal_to_mismatch(#[case] lhs: Value, #[case] rhs: Value, #[case] expected: ExpressionError) {
+        let result = evaluate(&NotEqualTo(Box::new(Literal(lhs)), Box::new(Literal(rhs))), &context()).unwrap_err();
+        assert_eq!(result, expected);
+    }
+
+    #[rstest]
+    #[case::unknown_device("unknown_device_id", "", Err(ExpressionError::UnknownDevice("unknown_device_id".to_string())))]
+    #[case::unknown_property("ab917a9a-a7d5-4853-9518-75909236a182", "unknown_property_id", Err(UnknownProperty { device_id: "ab917a9a-a7d5-4853-9518-75909236a182".to_string(), property_id: "unknown_property_id".to_string() }))]
     #[case::boolean("ab917a9a-a7d5-4853-9518-75909236a182", "on", Ok(Value::Boolean(true)))]
     #[case::number("ab917a9a-a7d5-4853-9518-75909236a182", "brightness", Ok(Value::Number(Number::Float(58.89))))]
-    fn property_value(#[case] device_id: &str, #[case] property_id: &str, #[case] expected: Result<Value, &str>) {
+    #[case::color("ab917a9a-a7d5-4853-9518-75909236a182", "color", Err(ExpressionError::UnsupportedPropertyType(PropertyType::Color)))]
+    #[case::color_temperature(
+        "ab917a9a-a7d5-4853-9518-75909236a182",
+        "colorTemperature",
+        Err(ExpressionError::UnsupportedPropertyType(PropertyType::ColorTemperature))
+    )]
+    fn property_value(#[case] device_id: &str, #[case] property_id: &str, #[case] expected: Result<Value, ExpressionError>) {
         let device = device();
         let devices: DeviceMap = HashMap::from([(device.id.clone(), Arc::new(device))]);
         let snapshot = StoreSnapshot { devices: Arc::new(devices) };
